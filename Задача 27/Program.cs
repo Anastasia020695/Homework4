5e704c58ed76027192c11016420eeec8ed9aed24
Anastasia <miss.markina2012@mail.rum>
2023-10-03 18:34:23 +0300

    Решение задач

--- Задача 27/Program.cs
+++ Задача 27/Program.cs
@@ -2,3 +2,27 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int len = Length(a);
+int Length(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+        }
+    return index;
+}
+int Sum(int a, int len)
+{
+    int sum = 0;
+    for (int i = len; i >= 1; i--)
+    {
+        sum += a % 10;
+        a /= 10;
+        }
+return sum;
+}
+Console.Write($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
